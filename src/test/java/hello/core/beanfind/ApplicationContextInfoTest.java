277b855afd2c196a71c33b453b10815706e355a3
KSB-skyBlueLover <gogotkdqja@yonsei.ac.kr>
2022-05-02 16:44:32 +0900

    use spring DI container

---
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,44 @@
+package hello.core.beanfind;
+
+import hello.core.config.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 bean 출력하기")
+    void findAllBean() {
+
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("beanDefinitionName = " + beanDefinitionName +
+                    " object = " + bean);
+        }
+
+    }
+
+    @Test
+    @DisplayName("모든 application bean 출력하기")
+    void findApplicationBean() {
+
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            // ROLE_APPLICATION: 직접 등록한 application bean
+            // ROLE_INFRASTRUCTURE: spring이 내부에서 사용하는 bean
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("beanDefinitionName = " + beanDefinitionName +
+                        " object = " + bean);
+            }
+        }
+
+    }
+}
